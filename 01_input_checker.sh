d1f83f9ffedba835788c434f6d5a0545024dfaf3
gitgauravchawla <46218683+gitgauravchawla@users.noreply.github.com>
2022-10-08 21:27:39 +0530

    Update 01_input_checker.sh

--- 01_input_checker.sh
+++ 01_input_checker.sh
@@ -20,3 +20,8 @@ if [ $# -gt 3 ] && [ $# -lt 3 ]; then
 #My mistakes/Learnings##
 # 1. after each "if" there has to be a "fi", Although i was nesting the if's i was not placing an "fi" after each "if".##
 ## 2. I was missing a space after "if" and before "[" in line #9. due to which i got error "expecting ..".
+
+##usefull links##
+##https://www.adminschoice.com/bash-positional-parameters#google_vignette##
+##https://acloudguru.com/blog/engineering/conditions-in-bash-scripting-if-statements#:~:text=Arithmetic%20(number%2Dbased)%20conditions%3A&text=NUM1%20is%20Greater%20Than%20NUM2.&text=NUM1%20is%20Greater%20than%20or%20Equal%20to%20NUM2.&text=NUM1%20is%20Less%20Than%20NUM2.&text=NUM1%20is%20Less%20than%20or%20Equal%20to%20NUM2.##
+##https://stackoverflow.com/questions/11622571/syntax-error-then-unexpected-expecting-fi##
